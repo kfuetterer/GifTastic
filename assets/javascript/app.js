14085fbdd088f9757a6ec60c65b46fca55bb4a6b
Katharina Fuetterer <Katharina Fuetterer>
2017-03-10 21:27:59 -0500

    Update 03/10/2017 9:27

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,9 @@
 window.onload = function () {
 
+//creating an array of animals, later will push new button names to this array
 var topics = ["monkeys", "dogs", "cats", "alligators", "turtles", "bunnies"];
 
 function displayGif() {
-	$("#gifs-appear-here").empty();
-
     var gif = $(this).attr("data-name");
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=dc6zaTOxFJmzC&limit=10";
     $(".animals").on("click", function() {
@@ -15,17 +14,24 @@ function displayGif() {
 	        var results = response.data;
 
 	        for (var i = 0; i < results.length; i++) {
-	            var gifDiv = $("<div class='item'>");
+				//getting the results of rating for each gif result and storing it in the variable rating
 	            var rating = results[i].rating;
+				//creating a paragraph tag with the rating inside
 	            var p = $("<p>").text("Rating: " + rating);
+				//creating an animalImage variable with an image tag. All gifs will be stored in this variable
 	            var animalImage = $("<img>");
+				//adding the attribute source and it's source url to the image tag
 	            animalImage.attr("src", results[i].images.fixed_height_still.url);
+				//adding the attribute data-animate with the url for the animated gif
 				animalImage.attr("data-animate", results[i].images.fixed_height.url);
+				//adding the attribute data-still with the url for the still gif
 				animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+				//adding the attribute data-state that lets us know if the gif is still or animated
 				animalImage.attr("data-state", "still");
-	            $("#gifs-appear-here").prepend(gifDiv);
-	            gifDiv.prepend(p);
-	            gifDiv.prepend(animalImage); 
+				//prepending the animalImage to the id "gifs-appear-here" in the index.html file
+	            $("#gifs-appear-here").prepend(animalImage);
+				//prepending the paragraph tag to the id "gifs-appear-here" in the index.html file
+	            $("#gifs-appear-here").append(p);
 	        }
 	    });
 	});
@@ -54,27 +60,25 @@ $("#add-gif").on("click", function(event) {
 
 $("#gifs-appear-here").on("click", "img", function(event) {
 
+	//if the data-state attribute of img equals to "still", therefore it is still
 	if ($(this).attr("data-state") === "still") {
+		//then find the source tag of img and replace it with the source in data-animate, ie make it animate
 		$(this).attr("src", $(this).attr("data-animate"));
+		//change the data-state to animate
 		$(this).attr("data-state", "animate");
 	}
 	
+	//if the data-state attribute of img equals to "animate", therefore it is animated
 	if ($(this).attr("data-state") === "animate") {
+		//then find the source tag of img and replace it with the source in data-still, ie make it still
 		$(this).attr("src", $(this).attr("data-still"));
+		//change the data-state to still
 		$(this).attr("data-state", "still");
 	}
 });
 
+//everytime a button is clicked with the class animals run the function displayGif, ie display the ten gifs
 $(document).on("click", ".animals", displayGif);
 renderButtons();
 
 }
-
-//Your app should take the topics in this array and create buttons in your HTML.
-//Try using a loop that appends a button for each string in the array.
-//When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
-//When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
-//Under every gif, display its rating (PG, G, so on).
-//This data is provided by the GIPHY API.
-//Only once you get images displaying with button presses should you move on to the next step.
-//Add a form to your page takes the value from a user input box and adds it into your topics array. Then make a function call that takes each topic in the array remakes the buttons on the page.
